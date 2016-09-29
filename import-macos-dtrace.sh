80255a874dc6cf69c3c8c0566f3399a4d4c5edd7
George Neville-Neil <gnn@neville-neil.com>
2016-09-29 11:13:02 +0100

    Add the import scripts for the xnu and dtrace tarballs from Apple
    Sponsored by: DARPA, AFRL

---
+++ import-macos-dtrace.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Author: George V. Neville-Neil
+#
+# Import an Apple DTrace tarball into the OpenDTrace git repo.
+#
+# This must be run from the root of the repo and there has to be a
+# base revision already checked in.
+#
+
+tarball=$1
+os_version=$2
+
+# Prep for a new import
+rm -rf *
+
+tar --strip-components 1 -xf ../../tarballs/opensource.apple.com/tarballs/dtrace/${tarball}.tar.gz
+
+git add .
+
+git commit -m "OSX: $os_version, Tar File: $tarball"
+
+# Tag this commit
+git tag $os_version
+git tag $tarball
+
